438e08b8af451beeac00db64319b856d5bb0d678
GunreddySrikanthreddy07 <gunreddysrikanthreddy07@gmail.com>
2023-05-31 18:46:58 +0530

    changes done

---
+++ subtracion.java
@@ -0,0 +1,14 @@
+package subtractionproj;
+
+public class subtracion {
+
+	public static void main(String[] args) {
+		int num1=2;
+		int num2=5;
+		int result;
+		result=num1-num2;
+		System.out.println(result);
+
+	}
+
+}
